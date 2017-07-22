e711635f44efb8a5d7fa4a35c3847dacf52835d8
James Hale <jmhale08@gmail.com>
2017-07-22 12:43:10 -0400

    Add session cookie and basic apps list

--- okta_auth.py
+++ okta_auth.py
@@ -20,14 +20,16 @@ class OktaAuth(object):
             "answer": factor_answer
         }
         post_url = "%s/api/v1/authn/factors/%s/verify" % (self.base_url, factor_id)
-        resp = requests.post(post_url, json=req_data)
-        print resp.json()
+        resp = requests.post(post_url, json=req_data).json()
+        if resp['status'] == "SUCCESS":
+            return resp['sessionToken']
+
 
 
     def verify_mfa(self, factors_list, state_token):
         """ Performs MFA auth against Okta """
         if len(factors_list) == 1:
-            self.verify_single_factor(factors_list[0]['id'], state_token)
+            session_token = self.verify_single_factor(factors_list[0]['id'], state_token)
         else:
             print "Registered MFA factors:"
 
@@ -47,7 +49,10 @@ class OktaAuth(object):
 
                 print "%d - %s"%(index+1, factor_name)
             factor_choice = input('Please select the MFA factor: ')-1
-            self.verify_single_factor(factors_list[factor_choice]['id'], state_token)
+            session_token = self.verify_single_factor(factors_list[factor_choice]['id'],
+                                                      state_token)
+
+        return session_token
 
 
     def primary_auth(self):
@@ -62,4 +67,20 @@ class OktaAuth(object):
         if resp['status'] == 'MFA_REQUIRED':
             factors_list = resp['_embedded']['factors']
             state_token = resp['stateToken']
-            self.verify_mfa(factors_list, state_token)
+            session_token = self.verify_mfa(factors_list, state_token)
+
+        session_id = self.get_session(session_token)
+        self.get_apps(session_id)
+
+    def get_session(self, session_token):
+        """ Gets a session cookie from a session token """
+        data = {"sessionToken": session_token}
+        resp = requests.post(self.base_url+'/api/v1/sessions', json=data).json()
+        return resp['id']
+
+    def get_apps(self, session_id):
+        """ Gets apps for the user """
+        sid = "sid=%s" % session_id
+        headers = {'Cookie': sid}
+        resp = requests.get(self.base_url+'/api/v1/users/me/appLinks', headers=headers)
+        print resp.json()
