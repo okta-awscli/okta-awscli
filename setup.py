ebfa3fc122169a6bd7632b638d70fce2ee53426b
James Hale <jmhale08@gmail.com>
2017-07-23 12:53:08 -0400

    Update for PyPi

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='okta-awscli',
-    version='0.1.0',
-    packages=['okta-awscli',],
+    version='0.1.1',
+    description='Provides a wrapper for Okta authentication to awscli',
+    packages=find_packages(),
     license='Apache License 2.0',
-    long_description=open('README.md').read(),
+    author='James Hale',
+    author_email='james@jameshale.me',
+    url='https://github.com/jmhale/okta_awscli',
+    entry_points={
+        'console_scripts': [
+            'okta-awscli=oktaawscli.okta_awscli:main',
+        ],
+    },
+    install_requires=[
+        'requests',
+        'click',
+        'bs4',
+        'boto3'
+        ],
 )
