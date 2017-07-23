4dca818a62c58231e53920622c728a986a72d4ca
James Hale <jmhale08@gmail.com>
2017-07-23 09:10:41 -0400

    Re-arrange for packaging

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='okta-awscli',
+    version='0.1.0',
+    packages=['okta-awscli',],
+    license='Apache License 2.0',
+    long_description=open('README.md').read(),
+)
