d97510b1677ccbaade46be6de3412731772791e5
REN Xiaojun <xiaojunr@aconex.com>
2018-03-09 16:36:49 +1100

    Fix bug: when lookup factor list by index, the factor_choice should be changed to zero based

--- oktaawscli/okta_auth.py
+++ oktaawscli/okta_auth.py
@@ -112,10 +112,10 @@ class OktaAuth(object):
                 else:
                     print("%d: %s" % (index + 1, factor_name))
             if not self.factor:
-                factor_choice = input('Please select the MFA factor: ')
+                factor_choice = input('Please select the MFA factor: ') - 1
             self.logger.info("Performing secondary authentication using: %s" %
                              supported_factors[factor_choice]['provider'])
-            session_token = self.verify_single_factor(supported_factors[factor_choice-1],
+            session_token = self.verify_single_factor(supported_factors[factor_choice],
                                                       state_token)
         else:
             print("MFA required, but no supported factors enrolled! Exiting.")
