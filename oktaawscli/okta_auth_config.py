ea925bcfc8b688da8c04a430427dd1d9db2d2484
James Hale <james@jameshale.net>
2018-07-20 22:18:58 -0400

    Various linting fixes

--- oktaawscli/okta_auth_config.py
+++ oktaawscli/okta_auth_config.py
@@ -1,10 +1,17 @@
+""" Config helper """
+
 import os
 
 from configparser import RawConfigParser
 from getpass import getpass
 
+try:
+    input = raw_input
+except NameError:
+    pass
 
-class OktaAuthConfig(object):
+class OktaAuthConfig():
+    """ Config helper class """
     def __init__(self, logger):
         self.logger = logger
         self.config_path = os.path.expanduser('~') + '/.okta-aws'
@@ -12,6 +19,7 @@ class OktaAuthConfig(object):
         self._value.read(self.config_path)
 
     def base_url_for(self, okta_profile):
+        """ Gets base URL from config """
         if self._value.has_option(okta_profile, 'base-url'):
             base_url = self._value.get(okta_profile, 'base-url')
             self.logger.info("Authenticating to: %s" % base_url)
@@ -21,14 +29,16 @@ class OktaAuthConfig(object):
         return base_url
 
     def username_for(self, okta_profile):
+        """ Gets username from config """
         if self._value.has_option(okta_profile, 'username'):
             username = self._value.get(okta_profile, 'username')
             self.logger.info("Authenticating as: %s" % username)
         else:
-            username = raw_input('Enter username: ')
+            username = input('Enter username: ')
         return username
 
     def password_for(self, okta_profile):
+        """ Gets password from config """
         if self._value.has_option(okta_profile, 'password'):
             password = self._value.get(okta_profile, 'password')
         else:
@@ -36,12 +46,15 @@ class OktaAuthConfig(object):
         return password
 
     def factor_for(self, okta_profile):
+        """ Gets factor from config """
         if self._value.has_option(okta_profile, 'factor'):
             factor = self._value.get(okta_profile, 'factor')
             self.logger.debug("Setting MFA factor to %s" % factor)
             return factor
+        return None
 
     def save_chosen_role_for_profile(self, okta_profile, role_arn):
+        """ Gets role from config """
         if not self._value.has_section(okta_profile):
             self._value.add_section(okta_profile)
 
