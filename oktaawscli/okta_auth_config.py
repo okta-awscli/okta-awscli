229d93a3e1fcd390498df9b3e84a971564e18213
Johnathan Constance <jconstance@amplify.com>
2019-05-17 18:11:15 -0400

    Update defaulting config accessors to take advantage of configparser's defaulting

--- oktaawscli/okta_auth_config.py
+++ oktaawscli/okta_auth_config.py
@@ -20,89 +20,73 @@ class OktaAuthConfig():
 
     def base_url_for(self, okta_profile):
         """ Gets base URL from config """
-        if self._value.has_option(okta_profile, 'base-url'):
-            base_url = self._value.get(okta_profile, 'base-url')
-            self.logger.info("Authenticating to: %s" % base_url)
-        else:
-            base_url = self._value.get('default', 'base-url')
-            self.logger.info("Using base-url from default profile %s" % base_url)
+        base_url = self._value.get(okta_profile, 'base-url')
+        self.logger.info("Authenticating to: %s" % base_url)
         return base_url
 
     def username_for(self, okta_profile):
         """ Gets username from config """
-        if self._value.has_option(okta_profile, 'username') and not self.reset:
-            username = self._value.get(okta_profile, 'username')
-            self.logger.info("Authenticating as: %s" % username)
-        else:
+        username = self._value.get(okta_profile, 'username', fallback=None)
+        if not username or self.reset:
             username = getuser()
             entered_username = input('Enter username [%s]: ' % username)
-            username = username if entered_username == "" else entered_username
+            username = entered_username or username
+        self.logger.info("Authenticating as: %s" % username)
         return username
 
     def password_for(self, okta_profile):
         """ Gets password from config """
-        if self._value.has_option(okta_profile, 'password'):
-            password = self._value.get(okta_profile, 'password')
-        else:
+        password = self._value.get(okta_profile, 'password', fallback=None)
+        if not password:
             password = getpass('Enter password: ')
         return password
 
     def factor_for(self, okta_profile):
         """ Gets factor from config """
-        if self._value.has_option(okta_profile, 'factor') and not self.reset:
-            factor = self._value.get(okta_profile, 'factor')
-            self.logger.debug("Setting MFA factor to %s" % factor)
-            return factor
-        return None
+        if self.reset:
+            return None
+
+        factor = self._value.get(okta_profile, 'factor', fallback=None)
+        self.logger.debug("Setting MFA factor to %s" % factor)
+        return factor
 
     def app_for(self, okta_profile):
         """ Gets app from config """
-        if self._value.has_option(okta_profile, 'app') and not self.reset:
-            app = self._value.get(okta_profile, 'app')
-            self.logger.debug("Setting app to %s" % app)
-            return app
-        return None
+        if self.reset:
+            return None
+
+        app = self._value.get(okta_profile, 'app', fallback=None)
+        self.logger.debug("Setting app to %s" % app)
+        return app
 
     def region_for(self, okta_profile):
         """ Gets region from config """
-        if self._value.has_option(okta_profile, 'region'):
-            region = self._value.get(okta_profile, 'region')
-            self.logger.debug("Setting region to %s" % region)
-            return region
-        return 'us-east-1'
+        region = self._value.get(okta_profile, 'region', fallback="us-east-1")
+        self.logger.debug("Setting region to %s" % region)
+        return region
 
     def get_check_valid_creds(self, okta_profile):
         """ Gets if should check if AWS creds are valid from config """
-        check_valid_creds = "True"
-        if self._value.has_option(okta_profile, 'check-valid-creds'):
-            check_valid_creds = self._value.get(okta_profile, 'check-valid-creds')
-
+        check_valid_creds = self._value.get(okta_profile, 'check-valid-creds', fallback="True")
         self.logger.info("Check if credentials are valid: %s" % check_valid_creds)
         return check_valid_creds
 
     def get_store_role(self, okta_profile):
         """ Gets if should store role to okta-profile from config """
-        store_role = "True"
-        if self._value.has_option(okta_profile, 'store-role'):
-            store_role = self._value.get(okta_profile, 'store-role')
-
+        store_role = self._value.get(okta_profile, 'store-role', fallback="True")
         self.logger.info("Should store role: %s" % store_role)
         return store_role
 
     def get_auto_write_profile(self, okta_profile):
         """ Gets if should auto write aws creds to ~/.aws/credentials from config """
-        auto_write_profile = "False"
-        if self._value.has_option(okta_profile, 'auto-write-profile'):
-            auto_write_profile = self._value.get(okta_profile, 'auto-write-profile')
-
+        auto_write_profile = self._value.get(okta_profile, 'auto-write-profile', fallback=True)
         self.logger.info("Should write profile to ~/.aws/credentials: %s" % auto_write_profile)
         return auto_write_profile
 
     def get_session_duration(self, okta_profile):
         """ Gets STS session duration from config as an int"""
-        session_duration = 3600 # AWS docs say default duration is 1 hour (3600 seconds)
-        if self._value.has_option(okta_profile, 'session-duration'):
-            session_duration = int(self._value.get(okta_profile, 'session-duration'))
+        # AWS docs say default duration is 1 hour (3600 seconds)
+        session_duration = int(self._value.get(okta_profile, 'session-duration', fallback="3600"))
 
         if session_duration > 43200 or session_duration < 3600:
             self.logger.info("Invalid session duration specified, defaulting to 1 hour.")
