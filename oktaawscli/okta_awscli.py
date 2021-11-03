7aa26105bb4a951a0eb00435e0c1cc64b3738ffd
Daniel Xia <daniel.xia@elemy.com>
2021-11-03 15:11:36 -0700

    Fix - Load AWS profile from config if not provided as CLI arg

--- oktaawscli/okta_awscli.py
+++ oktaawscli/okta_awscli.py
@@ -112,6 +112,7 @@ def main(okta_profile, profile, verbose, version,
         okta_profile = "default"
 
     aws_auth = AwsAuth(profile, okta_profile, lookup, verbose, logger)
+    profile = aws_auth.profile
     if not aws_auth.check_sts_token(profile) or force:
         if force and profile:
 
