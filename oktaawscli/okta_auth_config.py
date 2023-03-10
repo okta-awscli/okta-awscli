2d3567afac91fa71a2e03e64f80a0f3dcd345e19
Keli Bezhani <ebezhani@amplify.com>
2023-03-10 14:46:39 -0500

    Added handling other statuses of Okta authentication; format with python black

--- oktaawscli/okta_auth_config.py
+++ oktaawscli/okta_auth_config.py
@@ -10,24 +10,26 @@ except NameError:
     pass
 
 
-DEFAULT_SECTION = 'default'
+DEFAULT_SECTION = "default"
 
 
-class OktaAuthConfig():
-    """ Config helper class """
+class OktaAuthConfig:
+    """Config helper class"""
+
     def __init__(self, logger, reset):
         self.logger = logger
         self.reset = reset
-        self.config_path = os.path.expanduser('~') + '/.okta-aws'
+        self.config_path = os.path.expanduser("~") + "/.okta-aws"
         self._value = SafeConfigParser(default_section=DEFAULT_SECTION)
         self._value.read(self.config_path)
 
     def base_url_for(self, okta_profile):
-        """ Gets base URL from config """
-        base_url = self._value.get(okta_profile, 'base-url', fallback=None)
+        """Gets base URL from config"""
+        base_url = self._value.get(okta_profile, "base-url", fallback=None)
         if self.reset or not base_url:
             entered_base_url = input(
-                'Enter Okta Base URL in form of "%s" [%s]: ' % (
+                'Enter Okta Base URL in form of "%s" [%s]: '
+                % (
                     "<your_okta_org>.okta.com",
                     base_url or "",
                 )
@@ -42,97 +44,107 @@ class OktaAuthConfig():
         return base_url
 
     def username_for(self, okta_profile):
-        """ Gets username from config """
-        username = self._value.get(okta_profile, 'username', fallback=None)
+        """Gets username from config"""
+        username = self._value.get(okta_profile, "username", fallback=None)
         if self.reset or not username:
             username = getuser()
-            entered_username = input('Enter username [%s]: ' % username)
+            entered_username = input("Enter username [%s]: " % username)
             username = entered_username or username
         self.logger.info("Authenticating as: %s" % username)
         return username
 
     def password_for(self, okta_profile):
-        """ Gets password from config """
-        password = self._value.get(okta_profile, 'password', fallback=None)
+        """Gets password from config"""
+        password = self._value.get(okta_profile, "password", fallback=None)
         if not password:
-            password = getpass('Enter password: ')
+            password = getpass("Enter password: ")
         return password
 
     def factor_for(self, okta_profile):
-        """ Gets factor from config """
+        """Gets factor from config"""
         if self.reset:
             return None
 
-        factor = self._value.get(okta_profile, 'factor', fallback=None)
+        factor = self._value.get(okta_profile, "factor", fallback=None)
         self.logger.debug("Setting MFA factor to %s" % factor)
         return factor
 
     def app_for(self, okta_profile):
-        """ Gets app from config """
+        """Gets app from config"""
         if self.reset:
             return None
 
-        app = self._value.get(okta_profile, 'app', fallback=None)
+        app = self._value.get(okta_profile, "app", fallback=None)
         self.logger.debug("Setting app to %s" % app)
         return app
 
     def region_for(self, okta_profile, default="us-east-1"):
-        """ Gets region from config """
-        region = self._value.get(okta_profile, 'region', fallback=default)
+        """Gets region from config"""
+        region = self._value.get(okta_profile, "region", fallback=default)
         self.logger.debug("Setting region=%s from section=%s", region, okta_profile)
         return region
 
     def get_check_valid_creds(self, okta_profile):
-        """ Gets if should check if AWS creds are valid from config """
-        check_valid_creds = self._value.get(okta_profile, 'check-valid-creds', fallback="True")
+        """Gets if should check if AWS creds are valid from config"""
+        check_valid_creds = self._value.get(
+            okta_profile, "check-valid-creds", fallback="True"
+        )
         self.logger.info("Check if credentials are valid: %s" % check_valid_creds)
         return check_valid_creds
 
     def get_store_role(self, okta_profile):
-        """ Gets if should store role to okta-profile from config """
-        store_role = self._value.get(okta_profile, 'store-role', fallback="True")
+        """Gets if should store role to okta-profile from config"""
+        store_role = self._value.get(okta_profile, "store-role", fallback="True")
         self.logger.info("Should store role: %s" % store_role)
         return store_role
 
     def get_auto_write_profile(self, okta_profile):
-        """ Gets if should auto write aws creds to ~/.aws/credentials from config """
-        auto_write_profile = self._value.get(okta_profile, 'auto-write-profile', fallback=True)
-        self.logger.info("Should write profile to ~/.aws/credentials: %s" % auto_write_profile)
+        """Gets if should auto write aws creds to ~/.aws/credentials from config"""
+        auto_write_profile = self._value.get(
+            okta_profile, "auto-write-profile", fallback=True
+        )
+        self.logger.info(
+            "Should write profile to ~/.aws/credentials: %s" % auto_write_profile
+        )
         return auto_write_profile
 
     def get_session_duration(self, okta_profile):
-        """ Gets STS session duration from config as an int"""
+        """Gets STS session duration from config as an int"""
         # AWS docs say default duration is 1 hour (3600 seconds)
-        session_duration = int(self._value.get(okta_profile, 'session-duration', fallback="3600"))
+        session_duration = int(
+            self._value.get(okta_profile, "session-duration", fallback="3600")
+        )
 
         if session_duration > 43200 or session_duration < 3600:
-            self.logger.info("Invalid session duration specified, defaulting to 1 hour.")
+            self.logger.info(
+                "Invalid session duration specified, defaulting to 1 hour."
+            )
             session_duration = 3600
 
         self.logger.info("Configured session duration: %s seconds" % session_duration)
         return session_duration
 
     def save_chosen_role_for_profile(self, okta_profile, role_arn):
-        """ Saves role to config """
+        """Saves role to config"""
         self._save_config_value(
             section=okta_profile,
-            key='role',
+            key="role",
             value=role_arn,
         )
 
     def save_chosen_factor_for_profile(self, okta_profile, factor):
-        """ Saves factor to config """
+        """Saves factor to config"""
         self._save_config_value(
             section=okta_profile,
-            key='factor',
+            key="factor",
             value=factor,
         )
 
     def save_chosen_app_for_profile(self, okta_profile, app):
-        """ Saves app to config """
+        """Saves app to config"""
         self._save_config_value(
             section=okta_profile,
-            key='app',
+            key="app",
             value=app,
         )
 
@@ -144,5 +156,5 @@ class OktaAuthConfig():
 
         self._value.set(section, key, value)
 
-        with open(self.config_path, 'w+') as configfile:
+        with open(self.config_path, "w+") as configfile:
             self._value.write(configfile)
--- oktaawscli/okta_awscli.py
+++ oktaawscli/okta_awscli.py
@@ -10,9 +10,22 @@ from oktaawscli.okta_auth_config import OktaAuthConfig
 from oktaawscli.aws_auth import AwsAuth
 
 
-def get_credentials(okta_profile, profile, account, write_default, verbose, logger,
-                    totp_token, cache, export, reset, force, region, debug=False):
-    """ Gets credentials from Okta """
+def get_credentials(
+    okta_profile,
+    profile,
+    account,
+    write_default,
+    verbose,
+    logger,
+    totp_token,
+    cache,
+    export,
+    reset,
+    force,
+    region,
+    debug=False,
+):
+    """Gets credentials from Okta"""
     okta_auth_config = OktaAuthConfig(logger, reset)
 
     aws_auth = AwsAuth(
@@ -33,8 +46,9 @@ def get_credentials(okta_profile, profile, account, write_default, verbose, logg
             print("Copying AWS profile creds to default")
         exit(0)
 
-    okta = OktaAuth(okta_profile, verbose, logger,
-                    totp_token, okta_auth_config, debug=debug)
+    okta = OktaAuth(
+        okta_profile, verbose, logger, totp_token, okta_auth_config, debug=debug
+    )
 
     _, assertion = okta.get_assertion()
     role = aws_auth.choose_aws_role(assertion)
@@ -52,23 +66,25 @@ def get_credentials(okta_profile, profile, account, write_default, verbose, logg
 
     duration = okta_auth_config.get_session_duration(okta_profile)
     sts_token = aws_auth.get_sts_token(role_arn, principal_arn, assertion, duration)
-    access_key_id = sts_token['AccessKeyId']
-    secret_access_key = sts_token['SecretAccessKey']
-    session_token = sts_token['SessionToken']
-    print("Credentials valid for %s hours" % round(duration/3600, 1))
+    access_key_id = sts_token["AccessKeyId"]
+    secret_access_key = sts_token["SecretAccessKey"]
+    session_token = sts_token["SessionToken"]
+    print("Credentials valid for %s hours" % round(duration / 3600, 1))
     if (profile_name is None or export) and not write_default:
-        logger.info("Either profile name not given or export flag set, will output to console.")
-        exports = console_output(access_key_id, secret_access_key,
-                                 session_token, verbose)
+        logger.info(
+            "Either profile name not given or export flag set, will output to console."
+        )
+        exports = console_output(
+            access_key_id, secret_access_key, session_token, verbose
+        )
         if cache:
-            cache = open("%s/.okta-credentials.cache" %
-                         (os.path.expanduser('~'),), 'w')
+            cache = open("%s/.okta-credentials.cache" % (os.path.expanduser("~"),), "w")
             cache.write(exports)
             cache.close()
         exit(0)
     else:
         # Check okta config again for region, but now with manually chosen account alias
-        default_region = okta_auth_config.region_for('default')
+        default_region = okta_auth_config.region_for("default")
         okta_region = okta_auth_config.region_for(okta_profile, default=None)
         account_region = okta_auth_config.region_for(profile_name, default=None)
 
@@ -79,12 +95,16 @@ def get_credentials(okta_profile, profile, account, write_default, verbose, logg
             logger.debug("Setting region=%s via okta-profile=%s", region, okta_profile)
         elif account_region is not None and account_region != default_region:
             region = account_region
-            logger.debug("Setting region=%s via account profile=%s", region, profile_name)
+            logger.debug(
+                "Setting region=%s via account profile=%s", region, profile_name
+            )
         else:
             region = default_region
             logger.debug("Setting region=%s via defaults", region)
 
-        logger.info("Export flag not set, will write credentials to ~/.aws/credentials.")
+        logger.info(
+            "Export flag not set, will write credentials to ~/.aws/credentials."
+        )
         aws_auth.write_sts_token(
             profile=profile_name,
             access_key_id=access_key_id,
@@ -95,7 +115,7 @@ def get_credentials(okta_profile, profile, account, write_default, verbose, logg
         if write_default:
             print("Writing to default AWS profile")
             aws_auth.write_sts_token(
-                profile='default',
+                profile="default",
                 access_key_id=access_key_id,
                 secret_access_key=secret_access_key,
                 session_token=session_token,
@@ -103,64 +123,111 @@ def get_credentials(okta_profile, profile, account, write_default, verbose, logg
             )
         # Only print usage message if account argument wasn't specified
         elif account is None:
-            usage_msg = "".join([
-                "\nTo start using these temporary credentials, run:\n",
-                "\n export AWS_PROFILE=%s\n" % profile_name
-            ])
+            usage_msg = "".join(
+                [
+                    "\nTo start using these temporary credentials, run:\n",
+                    "\n export AWS_PROFILE=%s\n" % profile_name,
+                ]
+            )
             print(usage_msg)
         exit(0)
 
 
 def console_output(access_key_id, secret_access_key, session_token, verbose):
-    """ Outputs STS credentials to console """
+    """Outputs STS credentials to console"""
     if verbose:
         print("Use these to set your environment variables:")
-    exports = "\n".join([
-        "export AWS_ACCESS_KEY_ID=%s" % access_key_id,
-        "export AWS_SECRET_ACCESS_KEY=%s" % secret_access_key,
-        "export AWS_SESSION_TOKEN=%s" % session_token,
-        "export AWS_SECURITY_TOKEN=%s" % session_token
-    ])
+    exports = "\n".join(
+        [
+            "export AWS_ACCESS_KEY_ID=%s" % access_key_id,
+            "export AWS_SECRET_ACCESS_KEY=%s" % secret_access_key,
+            "export AWS_SESSION_TOKEN=%s" % session_token,
+            "export AWS_SECURITY_TOKEN=%s" % session_token,
+        ]
+    )
     print(exports)
 
     return exports
 
+
 # pylint: disable=R0913
 @click.command()
-@click.option('-v', '--verbose', is_flag=True, help='Enables verbose mode')
-@click.option('-w', '--write-default', is_flag=True, help='Writes to both default and account')
-@click.option('-V', '--version', is_flag=True,
-              help='Outputs version number and exits')
-@click.option('-d', '--debug', is_flag=True, help='Enables debug mode')
-@click.option('-f', '--force', is_flag=True, help='Forces new STS credentials. \
-Skips STS credentials validation.')
-@click.option('-r', '--reset', is_flag=True, help='Resets default values in ~/.okta-aws')
-@click.option('-e', '--export', is_flag=True, help='Outputs credentials to console instead \
-of writing to ~/.aws/credentials')
-@click.option('--okta-profile', help="Name of the profile to use in .okta-aws. \
-If none is provided, then the default profile will be used.\n")
-@click.option('--profile', help="Name of the profile to store temporary \
+@click.option("-v", "--verbose", is_flag=True, help="Enables verbose mode")
+@click.option(
+    "-w", "--write-default", is_flag=True, help="Writes to both default and account"
+)
+@click.option("-V", "--version", is_flag=True, help="Outputs version number and exits")
+@click.option("-d", "--debug", is_flag=True, help="Enables debug mode")
+@click.option(
+    "-f",
+    "--force",
+    is_flag=True,
+    help="Forces new STS credentials. \
+Skips STS credentials validation.",
+)
+@click.option(
+    "-r", "--reset", is_flag=True, help="Resets default values in ~/.okta-aws"
+)
+@click.option(
+    "-e",
+    "--export",
+    is_flag=True,
+    help="Outputs credentials to console instead \
+of writing to ~/.aws/credentials",
+)
+@click.option(
+    "--okta-profile",
+    help="Name of the profile to use in .okta-aws. \
+If none is provided, then the default profile will be used.\n",
+)
+@click.option(
+    "--profile",
+    help="Name of the profile to store temporary \
 credentials in ~/.aws/credentials. If profile doesn't exist, it will be \
-created. If omitted, credentials will output to console.\n")
-@click.option('-c', '--cache', is_flag=True, help='Cache the default profile credentials \
-to ~/.okta-credentials.cache\n')
-@click.option('-t', '--token', help='TOTP token from your authenticator app')
-@click.option('-a', '--account', help='Target account to authenticate to. \
-Also writes AWS credentials to default profile ')
-@click.option('-r', '--region', help='The AWS region to export credentials for')
-@click.argument('awscli_args', nargs=-1, type=click.UNPROCESSED)
-def main(okta_profile, profile, verbose, version, write_default,
-         debug, force, export, cache, awscli_args, token, reset, account, region):
-    """ Authenticate to awscli using Okta """
+created. If omitted, credentials will output to console.\n",
+)
+@click.option(
+    "-c",
+    "--cache",
+    is_flag=True,
+    help="Cache the default profile credentials \
+to ~/.okta-credentials.cache\n",
+)
+@click.option("-t", "--token", help="TOTP token from your authenticator app")
+@click.option(
+    "-a",
+    "--account",
+    help="Target account to authenticate to. \
+Also writes AWS credentials to default profile ",
+)
+@click.option("-r", "--region", help="The AWS region to export credentials for")
+@click.argument("awscli_args", nargs=-1, type=click.UNPROCESSED)
+def main(
+    okta_profile,
+    profile,
+    verbose,
+    version,
+    write_default,
+    debug,
+    force,
+    export,
+    cache,
+    awscli_args,
+    token,
+    reset,
+    account,
+    region,
+):
+    """Authenticate to awscli using Okta"""
     if version:
         print(__version__)
         exit(0)
     # Set up logging
-    logger = logging.getLogger('okta-awscli')
+    logger = logging.getLogger("okta-awscli")
     logger.setLevel(logging.DEBUG)
     handler = logging.StreamHandler()
     handler.setLevel(logging.WARN)
-    formatter = logging.Formatter('%(levelname)s - %(message)s')
+    formatter = logging.Formatter("%(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     if verbose:
         handler.setLevel(logging.INFO)
@@ -176,13 +243,24 @@ def main(okta_profile, profile, verbose, version, write_default,
     if region:
         logger.debug("Overwriting region to be %s", region)
     get_credentials(
-        okta_profile, profile, account, write_default, verbose, logger,
-        token, cache, export, reset, force, region, debug=debug
+        okta_profile,
+        profile,
+        account,
+        write_default,
+        verbose,
+        logger,
+        token,
+        cache,
+        export,
+        reset,
+        force,
+        region,
+        debug=debug,
     )
 
     if awscli_args:
-        cmdline = ['aws', '--profile', profile] + list(awscli_args)
-        logger.info('Invoking: %s', ' '.join(cmdline))
+        cmdline = ["aws", "--profile", profile] + list(awscli_args)
+        logger.info("Invoking: %s", " ".join(cmdline))
         call(cmdline)
 
 
